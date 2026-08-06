d828adb8ce0a88a44b3b1a880b038c0ec2736ab1
WilliamBossard <cepicsec@gmail.com>
2026-08-06 16:40:58 +0200

    Fix __init__.py which was accidentally cleared, bump version to 1.0.1

--- src/endstone_tombstone/__init__.py
+++ src/endstone_tombstone/__init__.py
@@ -1,1 +1,23 @@
+from endstone.plugin import Plugin
+from .listener import TombstoneListener
+from .manager import TombstoneManager
 
+class TombstonePlugin(Plugin):
+    api_version = "0.11"
+    prefix = "Tombstone"
+
+    def __init__(self):
+        super().__init__()
+        self.manager = None
+
+    def on_enable(self):
+        self.logger.info("Enabling Tombstone Plugin...")
+        self.manager = TombstoneManager(self)
+        self.register_events(TombstoneListener(self, self.manager))
+        self.logger.info("Tombstone Plugin enabled successfully!")
+
+    def on_disable(self):
+        self.logger.info("Disabling Tombstone Plugin...")
+        if self.manager:
+            self.manager.save_data()
+        self.logger.info("Tombstone Plugin disabled!")
